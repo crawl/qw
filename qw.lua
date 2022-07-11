8db0db5e633e31d4a050e9ccb7358f345adcfa73
gammafunk <gammafunk@gmail.com>
2022-07-11 00:43:01 -0500

    feat: Adjust utility for weapons at min delay
    Once we hit min delay, a utility of 0.2 meant that qw was unlikely to
    train weapon skills until many of its other skills were at 27. For
    extended, this is probably undervaluing weapon training. It would be
    good to turn our system of fixed utility based on mindelay into
    something that considered weapon damage, perhaps adapting our weapon
    utility calculation code used for item evaluation or using the new
    damage rating information available in the description. For now we
    increase the base utility from 0.2 to 0.5 for weapons at min delay.

--- qw.lua
+++ qw.lua
@@ -7939,7 +7939,7 @@ function skill_value(sk)
             return 0
         end
     elseif sk == wskill() then
-        return (at_min_delay() and 0.20 or 1.5)
+        return (at_min_delay() and 0.5 or 1.5)
     end
 end
 
